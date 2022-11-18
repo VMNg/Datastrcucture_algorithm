508e986d2ec623b2ceb40943fe69cf3366e15e54
Minh-neh <nguyenminh121201@gmail.com>
2022-11-18 19:50:39 +0700

    done

--- find_dup.cpp
+++ find_dup.cpp
@@ -1,12 +1,15 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Solution {
-public:
+   public:
     int result;
     int findDuplicate(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        for (auto it = nums.begin(); it!= nums.end(); it++)
-        {
-            if (*(it+1)==*(it))
-            {
+        for (auto it = nums.begin(); it != nums.end(); it++) {
+            if (*(it + 1) == *(it)) {
                 result = *it;
                 break;
             }
@@ -14,3 +17,10 @@ public:
         return result;
     }
 };
+
+int main() {
+    Solution s;
+    vector<int> nums = {1, 5, 2, 3, 1, 7};
+    cout << s.findDuplicate(nums);
+    return 0;
+}
